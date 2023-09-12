f52ccf4bb85d610b1402ee93830379ea9c63c053
el-sam <samurai.h@yahoo.com>
2023-09-12 16:15:39 +0800

    define function with the parameter has default value.

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
 function introduction(name){
     
-    let msg = (`Hi, my name is ${name}.`);
+    const msg = (`Hi, my name is ${name}.`);
+    return msg;
+    console.log(msg);
+    
+}
+
+// create a funciton to take 2 parameters
+function introductionWithLanguage(name, language){
+    const msg = `Hi, my name is ${name} and I am learning to program in ${language}.`;
+    return msg;
+
+}
+
+//create a function contain parameter with default value
+function introductionWithLanguageOptional(name, language = "JavaScript"){
+    const msg = `Hi, my name is ${name} and I am learning to program in ${language}.`;
     return msg;
 }
+
+
+
+introduction("Aki");
